539340352df9a01e42caf71cea917a7d7afa573d
Toni Almeida <promatik@gmail.com>
2019-05-24 12:30:18 +0100

    Added rule to friend card owners to be redirected to the friend card discounts

--- app/Http/Middleware/AdminPanelAccess.php
+++ app/Http/Middleware/AdminPanelAccess.php
@@ -16,6 +16,13 @@ class AdminPanelAccess
     public function handle($request, Closure $next)
     {
         if (!is(['admin', 'volunteer', 'store', 'translator'])) {
+
+            // Friend Card users
+            if (backpack_user() && backpack_user()->friend_card_modality()->first()) {
+                session(['login' => true]);
+                return redirect('/friends');
+            }
+
             abort(401);
         }
 
