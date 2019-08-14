25371847507fa27b188abe4b28e3849cf684348f
Toni Almeida <promatik@gmail.com>
2019-08-14 18:53:44 +0100

    Validated user has all the products when assigning new orders

--- app/Http/Requests/StoreOrderRequest.php
+++ app/Http/Requests/StoreOrderRequest.php
@@ -4,6 +4,8 @@ namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
 use App\Models\StoreOrder;
+use App\Models\StoreStock;
+use App\User;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreOrderRequest extends FormRequest
@@ -64,11 +66,52 @@ class StoreOrderRequest extends FormRequest
     {
         $validator->after(function ($validator) {
             $id = $this->input('id');
+            $user_id = $this->input('user_id');
 
             // Common user cannot edit a shipped order
             if (!is('admin', 'store orders') && StoreOrder::find($id)->shipment_date) {
                 return $validator->errors()->add('sent', __("You don't have the permissions to update an order already shipped."));
             }
+
+            // Validate user has all the products
+            $products = json_decode($this->input('products'));
+            foreach ($products as $product) {
+                $product_id = $product->id;
+                $product_quantity = $product->pivot->quantity;
+
+                // Stock quantity
+                $quantity_stock = StoreStock::where('user_id', $user_id)
+                    ->where('store_product_id', $product_id)
+                    ->sum('quantity');
+
+                // Sent quantity
+                $quantity_sent = StoreOrder::join('store_orders_products', 'store_orders_products.store_order_id', '=', 'store_orders.id')
+                    ->where('store_product_id', $product_id)
+                    ->where('user_id', $user_id);
+
+                // In case it is an edit, itens on that order doesn't count
+                if ($id) {
+                    $quantity_sent = $quantity_sent->where('id', '<>', $id);
+                }
+
+                $quantity_sent = $quantity_sent->sum('quantity');
+
+                // Total normalized
+                $total = max(0, $quantity_stock - $quantity_sent);
+
+                if ($product_quantity > $total) {
+                    $username = User::select('name')->where('id', $user_id)->first()->name;
+
+                    $validator->errors()->add('products', trans_choice(__('store_order_assing_error'), $total, [
+                        'amount' => $product_quantity,
+                        'product' => $product->name,
+                        'user' => $username,
+                        'quantity' => $total,
+                    ]));
+                }
+            }
+
+            return $validator->errors();
         });
     }
 }
