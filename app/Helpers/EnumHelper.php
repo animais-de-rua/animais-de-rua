c2ef3360bbfbe2786340121b6015a862a8de7dba
Mario Rocha <mario.rocha@infraspeak.com>
2025-03-10 18:43:45 +0000

    Add types on parameters and return types

--- app/Helpers/EnumHelper.php
+++ app/Helpers/EnumHelper.php
@@ -6,24 +6,26 @@ use Illuminate\Support\Facades\Config;
 
 class EnumHelper
 {
-    public static function get($name)
+    public static function get(string $name): mixed
     {
-        return Config::get("enums.$name");
+        return Config::get("enums.{$name}");
     }
 
-    public static function values($name, $join = null)
+    public static function values(string $name, null|array|string $join = null): array|string
     {
         $data = array_values(self::get($name));
+
         return $join ? join($join, $data) : $data;
     }
 
-    public static function keys($name, $join = null)
+    public static function keys(string $name, null|array|string $join = null): array|string
     {
         $data = array_keys(self::get($name));
+
         return $join ? join($join, $data) : $data;
     }
 
-    public static function translate($name)
+    public static function translate(string $name): array
     {
         $enum = [];
         foreach (self::get($name) as $key => $value) {
